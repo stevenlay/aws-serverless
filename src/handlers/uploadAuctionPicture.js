c552b391747a7ce837685fbc3b373c2d65941e90
stevenlay <imstevencoding@gmail.com>
2021-02-24 13:09:29 -0800

    upload picture

--- src/handlers/uploadAuctionPicture.js
+++ src/handlers/uploadAuctionPicture.js
@@ -1,13 +1,21 @@
 import middy from '@middy/core';
 import httpErrorHandler from '@middy/http-error-handler';
 import createError from 'http-errors';
+import validator from '@middy/validator';
 import { getAuctionById } from './getAuction';
 import { uploadPictureToS3 } from '../lib/uploadPictureToS3';
 import { updateAuctionPictureUrl } from '../lib/updateAuctionPictureUrl';
+import uploadAuctionPictureSchema from '../lib/schemas/uploadAuctionPictureSchema';
 
 async function uploadAuctionPicture(event, context) {
   const { id } = event.pathParameters;
+  const { email } = event.requestContext.authorizer;
   const auction = await getAuctionById(id);
+
+  if (auction.seller !== email) {
+    throw new createError.Forbidden('You are not the seller of the auction');
+  }
+
   const base64 = event.body.replace(/^data:image\/\w+;base64,/, '');
   const buffer = Buffer.from(base64, 'base64');
 
@@ -26,4 +34,6 @@ async function uploadAuctionPicture(event, context) {
   };
 }
 
-export const handler = middy(uploadAuctionPicture).use(httpErrorHandler());
+export const handler = middy(uploadAuctionPicture)
+  .use(httpErrorHandler())
+  .use(validator({ inputSchema: uploadAuctionPictureSchema }));
